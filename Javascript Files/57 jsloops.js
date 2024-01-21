3885b4076b4c0a1e05db6e8bf4cc5783d16250d9
Abhishek Gadad <abhishekgadad485@gmail.com>
2024-01-22 00:13:21 +0530

    JS Functions

--- Javascript Files/57 jsloops.js
+++ Javascript Files/57 jsloops.js
@@ -1,1 +1,41 @@
-console.log("Looping statements")
+// console.log("Looping statements")
+
+//  console.log("First for loop")
+
+//  let a = 1;
+//  for (let i = 0; i < 10; i++) {
+//     console.log(a+i); 
+// }
+
+// //For in loop
+
+// //example object with obj
+// let obj = {
+//     name : "Abhi",
+//     work : "programming",
+//     company : "aiet"
+// }
+// console.log("For in Loop");
+// for (const key in obj) {
+//     const ele = obj[key]
+//     console.log(key , ele)
+//     }
+// for (const c of "Abhishek"){
+//     console.log(c)
+// }
+
+//while Loop 
+// console.log("this is while loop")
+// let i = 0;
+// while(i<=5){
+//     console.log(i+1)
+//     i++;
+// }
+
+//DO WHILE
+let i = 0;
+do {
+    console.log(i+1)
+    i++;
+} while (i<6);
+
---
+++ Javascript Files/58 js functions.js
@@ -0,0 +1,22 @@
+console.log("Javascript functions")
+
+// function func(name)
+// {
+//     console.log("Hello," + name + "Welcome to our store")
+//     console.log("Hello," + name + "You got a discount")
+//     console.log("Hello," + name + "you got a ticket")
+//     console.log("Hello," + name + "you are awesome")
+//     console.log("Bye bye," + name + " come again")
+// }
+
+// func(" sujay ")
+
+function sum(a , b)
+{
+    console.log("Sum of two is :"+ (a+b))
+    console.log("difference of two no. is: " + (a-b))
+    console.log("Product of two no.is :"+(a*b))
+    console.log("Remainder of two no. is :"+(a%b))
+}
+
+sum(2,4)
