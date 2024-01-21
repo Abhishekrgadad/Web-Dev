dc5b9b959137202707d8ef33333e01f4fd6af8aa
Abhishek Gadad <abhishekgadad485@gmail.com>
2024-01-21 22:41:25 +0530

    Javascript

---
+++ Javascript Files/56 javascript.js
@@ -0,0 +1,26 @@
+console.log(" these are conditional statements");
+
+// let age = 5;
+// let grace = 2;
+
+// console.log( age + grace);
+
+// if(age-grace > 18) 
+//     console.log("YOU CAN DRIVE");
+// else
+//     console.log("YOU CAN'T RIDE");
+
+//Else if ladder
+
+// let age = 20;
+// if(age < 20)
+//     console.log("the person age is below 20")
+// else if (age==20)
+//     console.log("the person age is equal 20");
+// else
+//     console.log("the person   age is More and more")
+
+let a = 6
+let b = 8
+let c = a>b? (a-b):(a+b);
+console.log(c)
