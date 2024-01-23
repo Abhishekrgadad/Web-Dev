80a29fe55578159e27e6ca1904bd1254d0153e33
Abhishek Gadad <abhishekgadad485@gmail.com>
2024-01-23 22:18:56 +0530

    ARRAYS IN JS

--- Javascript Files/63 Arrays in JS.js
+++ Javascript Files/63 Arrays in JS.js
@@ -1,105 +1,130 @@
-Arrays in javascript
+// Arrays in javascript
 
-let a = [1, 2,3,4,5,6]
-console.log(a.length) //Length of the string
-console.log(a[2])
-console.log(a[3])
+// let a = [1, 2,3,4,5,6]
+// console.log(a.length) //Length of the string
+// console.log(a[2])
+// console.log(a[3])
 
-console.log(a , typeof a) //check type of data
+// console.log(a , typeof a) //check type of data
 
-console.log(a.toString(a)) //convert array to string
+// console.log(a.toString(a)) //convert array to string
 
-console.log(a.join(" and ")) //this function adds "and" by replacing comma
+// console.log(a.join(" and ")) //this function adds "and" by replacing comma
 
-console.log(a.pop()) //Which deletes the last element in the array
+// console.log(a.pop()) //Which deletes the last element in the array
 
-console.log(a.shift()) //Which deletes the first element from the array
+// console.log(a.shift()) //Which deletes the first element from the array
 
-console.log(a.push("harry")) //which inserts an element at the end
+// console.log(a.push("harry")) //which inserts an element at the end
 
-console.log( a.unshift("Abhishek")) //Which inserts array at first
-console.log(delete a[5]) 
+// console.log( a.unshift("Abhishek")) //Which inserts array at first
+// console.log(delete a[5]) 
 
-let a = [1,2,3]
-let b = [4,5,6]
-let c = [7,8,9]
-console.log(a.concat( b ,c ))  //Concatination of strings a,b ,c
+// let a = [1,2,3]
+// let b = [4,5,6]
+// let c = [7,8,9]
+// console.log(a.concat( b ,c ))  //Concatination of strings a,b ,c
 
-let s = [ 8,6,4,3,2,1]
-console.log(s.sort()) //Sorting of elements using sort function.
+// let s = [ 8,6,4,3,2,1]
+// console.log(s.sort()) //Sorting of elements using sort function.
 
-let  newone = [1,2,3,4,5,6,7,6]
-console.log(newone.splice(1,3)) //It delete the elements on the specific index
+// let  newone = [1,2,3,4,5,6,7,6]
+// console.log(newone.splice(1,3)) //It delete the elements on the specific index
 
-console.log(newone.splice(0 , 1 , 200 ,400)) //it delete the elements at the specific index and also add the specific number you given in the brackets.
-console.log(newone)
+// console.log(newone.splice(0 , 1 , 200 ,400)) //it delete the elements at the specific index and also add the specific number you given in the brackets.
+// console.log(newone)
 
-let  a = [1,2,3,4,5,6,7,6]
+// let  a = [1,2,3,4,5,6,7,6]
 
-//PRINTING ARRAY ELEMENTS
-for (let index = 0; index < a.length; index++) {
-    const element = a[index];
-    console.log(element)
+// //PRINTING ARRAY ELEMENTS
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
     
-}
+// }
 
-//FOR EACH LOOP
-a.forEach(value , index , arr)=>{
-    console.log(value , index , arr)
-}
+// //FOR EACH LOOP
+// a.forEach(value , index , arr)=>{
+//     console.log(value , index , arr)
+// }
 
 
-// FOR IN LOOP
-let object = {  
-    a : 1,
-     b : 2,
-     c : 4,
-}
-for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        console.log(element , key)
+// // FOR IN LOOP
+// let object = {  
+//     a : 1,
+//      b : 2,
+//      c : 4,
+// }
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+//         console.log(element , key)
         
-    }
-}
+//     }
+// }
 
-// DISPLAY ARRAYS USING FOR OF LOOP
-let arr = [1,2,43,4,5,5,6]
-for (const iterator of arr) {
-    console.log(iterator)
-}
-//CREATING A NEW ARRAY USING EXISTED ARRAY FOR LOOP
-let arr = [1,2,3,4,5]
-let newarr =[];
-for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    newarr.push(element **2);
+// // DISPLAY ARRAYS USING FOR OF LOOP
+// let arr = [1,2,43,4,5,5,6]
+// for (const iterator of arr) {
+//     console.log(iterator)
+// }
+// //CREATING A NEW ARRAY USING EXISTED ARRAY FOR LOOP
+// let arr = [1,2,3,4,5]
+// let newarr =[];
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     newarr.push(element **2);
     
+// }
+// console.log(newarr)
+
+// //CREATING A NEW ARRAY USING EXISTED ARRAY USING MAP FUNCTION
+// let arr = [1,2,3,4,5]
+// let newarr = arr.map((ele)=>{
+//     return ele**2
+// }
+// )
+// console.log(newarr)
+
+
+// //FILTER THE ELEMENTS FROM THE ARRAY BY CREATING A GREATER FUNCTION
+// let arr = [1,2,3,4,5]
+// const greater = (ele)=>{
+//     if(ele > 3)
+//         return true;
+//     return false;
+// }
+
+// console.log(arr.filter(greater))
+
+// //FILTER THE ELEMENTS FROM THE ARRAY WITHOUT USING FUNCTION.
+// console.log(arr.filter((ele)=> {
+//     if(ele < 5)
+//         return true
+//     return false
+// }))
+
+//REDUCE METHOD FOR MULTIPLICATION
+let arr2 = [2,3,4,5,6]
+const red = (a, b) =>{
+    return a*b
 }
-console.log(newarr)
+console.log(arr2.reduce(red))
 
-//CREATING A NEW ARRAY USING EXISTED ARRAY USING MAP FUNCTION
-let arr = [1,2,3,4,5]
-let newarr = arr.map((ele)=>{
-    return ele**2
+//REDUCE METHOD FOR ADDITION
+let arr3 = [2,3,4,5,6,7,8]
+const add = (a , b)=>{
+    return a+b;
 }
-)
-console.log(newarr)
+console.log(arr3.reduce(add))
 
-
-//FILTER THE ELEMENTS FROM THE ARRAY BY CREATING A GREATER FUNCTION
-let arr = [1,2,3,4,5]
-const greater = (ele)=>{
-    if(ele > 3)
-        return true;
-    return false;
+//REDUCE METHOD FOR SUBSTRACTION
+let arr4 = [100,20,30,10]
+const sub=(a , b)=>{
+    return a-b;
 }
+console.log(arr4.reduce(sub))
 
-console.log(arr.filter(greater))
-
-//FILTER THE ELEMENTS FROM THE ARRAY WITHOUT USING FUNCTION.
-console.log(arr.filter((ele)=> {
-    if(ele < 5)
-        return true
-    return false
-}))
+//ARRAY FROM USED TO MAKE ARRAY FROM STRING.
+let str = "harry"
+console.log(Array.from(str))
