8b81836f7cb14950ee7030d0feb17a89bac3b14c
Abhishek Gadad <abhishekgadad485@gmail.com>
2024-01-31 19:12:42 +0530

    ID'S AND CLASSES IN JS

---
+++ Javascript Files/68 idsandclass.js
@@ -0,0 +1,6 @@
+
+let cont = document.body.getElementsByClassName("box") //GET ALL THE CONTAINER LISTED HERE
+console.log(cont)
+
+document.body.getElementsByClassName //WHICH PROVIDES THE NAME OF THE EXACT ELEMENT BASED ON THE GIVEN CLASSNAME
+document.body.getElementsByTagName //THIS PROVIDES IDS BASED ON THE TAGNAMES
